2c4cdcfac5d5d21a34db4352aa89d3e5bc32bd9e
liaoxiaoyang3-prog <liaoxiaoyang3@gmail.com>
2026-06-24 01:18:53 +0800

    move mail configuration inside application factory

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,9 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+    # Force the email sender to remain silent and bypass network calls:
+    app.config['MAIL_SUPPRESS_SEND'] = True
     
     # 3. Initialize Extensions
     mail.init_app(app)
