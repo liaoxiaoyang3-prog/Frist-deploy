b03cdb5c952d047febbc764fc22cb40c54d9e766
liaoxiaoyang3-prog <liaoxiaoyang3@gmail.com>
2026-06-24 00:32:29 +0800

    remove unsupported ssl_mode argument

--- app/db.py
+++ app/db.py
@@ -2,11 +2,15 @@
 import os
 import mysql.connector
 
+import os
+import mysql.connector
+
 def get_db_connection():
+    # mysql-connector-python automatically negotiates SSL with Aiven by default
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 11780)), 
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        ssl_mode='REQUIRED' # Tells Python to connect securely to Aiven
+        database=os.getenv("DB_NAME")
     )
