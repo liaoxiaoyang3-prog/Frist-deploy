669061e1db6f10b932a82808ac393b2b278084c8
liaoxiaoyang3-prog <liaoxiaoyang3@gmail.com>
2026-06-24 01:12:50 +0800

    add mail suppress send to fix timeout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # main.py
 import os
-
 from app import create_app
-
 
 app = create_app()
 
@@ -12,6 +10,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+# ADD THIS LINE HERE to prevent the 30-second freeze:
+app.config['MAIL_SUPPRESS_SEND'] = True
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
